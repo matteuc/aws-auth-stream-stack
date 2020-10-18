c4ddb9a9e10855258ed8aea0258e49133aedd727
matteuc <ymc006@ucsd.edu>
2020-10-18 09:08:32 -0700

    script to zip lambda and layer

--- zip.js
+++ zip.js
@@ -1,11 +1,25 @@
 import zip from 'bestzip';
 
-zip({
-    source: './layer/nodejs',
-    destination: './bin/nodejs.zip'
-}).then(function () {
-    console.log('All done!');
-}).catch(function (err) {
-    console.error(err.stack);
-    process.exit(1);
-});
+const main = async () => {
+    try {
+        await Promise.all([
+            zip({
+                source: './nodejs',
+                destination: '../bin/nodejs.zip',
+                cwd: "./layer"
+            }),
+            zip({
+                source: './transcoder',
+                destination: '../bin/transcoder.zip',
+                cwd: "./lambda"
+            })
+        ])
+
+        console.log("All done!")
+    } catch (error) {
+        console.error(error.stack);
+        process.exit(1);
+    }
+}
+
+main()
